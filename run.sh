0e0d0561dc33101bceaddd45143d03f5b98b6fa4
Ben Kurtovic <wikipedia.earwig@gmail.com>
2013-08-24 03:37:36 -0400

    Have jobs store stdout/stderr in their own folder.

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-jsub -N $1 /data/project/mwp/python/bin/python /data/project/mwp/xmltester.py $1
+jsub -o job_logs -e job_logs -N $1 /data/project/mwp/python/bin/python /data/project/mwp/xmltester.py $1
